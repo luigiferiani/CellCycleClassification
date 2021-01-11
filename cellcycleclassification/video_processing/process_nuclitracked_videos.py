4121b0c90ab9eca7f1a3d6dc62f2c8057e85a538
Luigi Feriani <luigi.feriani@gmail.com>
2021-01-11 21:45:24 +0000

    process_nuclitracked_videos.py: bugfix for export_frames

--- cellcycleclassification/video_processing/process_nuclitracked_videos.py
+++ cellcycleclassification/video_processing/process_nuclitracked_videos.py
@@ -22,7 +22,7 @@ def csvfname2imgdir(csv_fname: Path):
     return csv_fname.parent / csv_fname.stem.replace('_sel', '')
 
 
-def process_directory(working_dir: Path, export_frames: Path = False):
+def process_directory(working_dir: Path, is_export_frames: bool = False):
     """
     Process the nuclitrack files in the input_path.
     On each video run inference with two pytorch models,
@@ -39,7 +39,7 @@ def process_directory(working_dir: Path, export_frames: Path = False):
         input_path/another_video_name_sel.csv -> nuclitrack csv
         input_path/another_video_name/        -> folder of frames
 
-    export_frames: Path, default True
+    is_export_frames: Bool, default False
         If True, export a preview of classified cells.
 
     Returns
@@ -54,10 +54,10 @@ def process_directory(working_dir: Path, export_frames: Path = False):
     print(f'Found {len(csv_fnames)} videos to classify.', flush=True)
 
     for csv_fname in tqdm(csv_fnames, desc='Processing video '):
-        process_one(csv_fname)
+        process_one(csv_fname, is_export_frames=is_export_frames)
 
 
-def process_one(tracking_csv: Path, export_frames: Path = False):
+def process_one(tracking_csv: Path, is_export_frames: bool = False):
     """
     Process the tracking_csv by running inference with two pytorch models,
     combining the outputs into a single prediction,
@@ -71,7 +71,7 @@ def process_one(tracking_csv: Path, export_frames: Path = False):
         /path/to/video_data_sel.csv -> the nuclitrack csv
         /path/to/video_data/        -> folder containing video frames as tiffs
 
-    export_frames: Path, default True
+    is_export_frames: Bool, default True
         If True, export a preview of classified cells.
 
     Returns
@@ -79,6 +79,7 @@ def process_one(tracking_csv: Path, export_frames: Path = False):
     None.
 
     """
+
     # input checks
     assert tracking_csv.name.endswith('_sel.csv'), (
         "Expecting a csv file ending in `_sel.csv`")
@@ -93,13 +94,13 @@ def process_one(tracking_csv: Path, export_frames: Path = False):
         )
     vidproc.process_video()
     vidproc.export_csv()
-    if export_frames:
+    if is_export_frames:
         vidproc.export_frames()
 
     return
 
 
-def process(input_path: Path, export_frames: Path = False):
+def process(input_path: Path, export_frames: bool = False):
     """
     Process the input_path (or the files in the input_path)
     by running inference with two pytorch models,
@@ -120,7 +121,7 @@ def process(input_path: Path, export_frames: Path = False):
         input_path/another_video_name_sel.csv -> nuclitrack csv
         input_path/another_video_name/        -> folder of frames
 
-    export_frames: Path, default True
+    export_frames: Bool, default False
         If True, export a preview of classified cells.
 
     Returns
@@ -133,9 +134,9 @@ def process(input_path: Path, export_frames: Path = False):
         input_path = Path(input_path)
     #   send to appropriate processing function
     if input_path.is_dir():
-        process_directory(input_path, export_frames=export_frames)
+        process_directory(input_path, is_export_frames=export_frames)
     else:
-        process_one(input_path, export_frames=export_frames)
+        process_one(input_path, is_export_frames=export_frames)
 
 
 def main():
